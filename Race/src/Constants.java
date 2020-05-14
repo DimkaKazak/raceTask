9a7843f98408852e635f6280b2950fd92809331c
DimkaKazak <Kazak211099@gmail.com>
2020-05-14 21:14:52 +0300

    add: Constants.java

---
+++ Race/src/Constants.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class Constants {
+
+    private static final String sep = File.separator;
+
+    public static final String POINTS_FILE_PATH = "Race" + sep + "files" + sep + "points";
+    public static final String PROPERTIES_FILE_PATH = "Race" + sep + "src" + sep + "config.properties";
+}
